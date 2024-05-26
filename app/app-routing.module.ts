1c1fcd0c5e5b65286672f00a5c838cc87c162c5a
Sanjid Rahman <rahmansanjid505@gmail.com>
2024-05-26 17:47:30 +0530

    made necessary changes in the routing module where chat module is lazy loaded and changes made in module because all the commmon modules are placed in the feature module

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ChatComponent } from './components/chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', title: 'Login', component: LoginComponent },
+  { path: 'login', redirectTo: '', pathMatch: 'full' },
+  { path: 'chat', loadChildren: () => import('./components/chat/chat.module').then(m => m.ChatModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,18 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChatComponent } from './components/chat/chat.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChatModule } from './components/chat/chat.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ChatModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
