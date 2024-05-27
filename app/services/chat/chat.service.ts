4335ae7ea66972c4e57061151e66bb0e71b3ca47
Sanjid Rahman <rahmansanjid505@gmail.com>
2024-05-28 02:10:21 +0530

    added all the socket events for group chat

--- app/services/chat/chat.service.ts
+++ app/services/chat/chat.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,11 @@ export class ChatService {
     private _socket: Socket,
   ) { }
 
-  joinGroup(group: string): void {
-    this._socket.emit('joinGroup', group);
+  commonUrl = environment.API_URL
+
+  reconfigureSocketConnection(token: string) {
+    this._socket.ioSocket.auth = { token };
+    this._socket.ioSocket.connect();
   }
 
   sendPrivateMessage(data: { recipientId: string, message: string }): void {
@@ -26,12 +30,41 @@ export class ChatService {
     return this._socket.fromEvent<any[]>('messageHistory');
   }
 
-  onPrivateMessage(): Observable<{ message?: string, senderId: string, createdAt: string, fileUrl?: string}> {
+  fetchGroupMessages(isPrivate: boolean) {
+    this._socket.emit('fetchGroupMessages', { isPrivate })
+    return this._socket.fromEvent<any[]>('messageHistory');
+  }
+
+  onPrivateMessage(): Observable<{ message?: string, senderId: string, createdAt: string, fileUrl?: string }> {
     return this._socket.fromEvent('privateMessage');
   }
 
   uploadFile(formData: FormData): Observable<any> {
-    return this._http.post('http://localhost:3000/api/upload', formData); // Update the URL to your backend API
+    return this._http.post(`${this.commonUrl}/api/upload`, formData); // Update the URL to your backend API
+  }
+
+  joinGroup(group: string) {
+    this._socket.emit('joinGroup', group);
+  }
+
+  leaveGroup(group: string) {
+    this._socket.emit('leaveGroup', group);
+  }
+
+  sendGroupMessage(data: { group: string, message: string }) {
+    this._socket.emit('groupMessage', data);
+  }
+
+  onGroupMessage() {
+    return this._socket.fromEvent('groupMessage');
+  }
+
+  onUserConnected(): Observable<{ message: string }> {
+    return this._socket.fromEvent('userConnected');
+  }
+
+  onUserDisconnected(): Observable<{ message: string }>{
+    return this._socket.fromEvent('userDisconnected');
   }
 
 }
